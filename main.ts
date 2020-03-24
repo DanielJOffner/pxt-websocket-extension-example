629a1c5d6ae0c25de41434dedd2e017ed5b4cb5e
unknown <daniel.offner@outlook.com>
2020-03-24 21:36:13 +1000

    3

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+function WsOpen(ev: Event){
+    console.log("websocket conneciton open");
+};
+
+
+function WsOnMessage(ev: Event) {
+    console.log(`Message receieved from websocket connection: ${ev.data}`);
+}
+
 /**
  * Functions to do basically nothing
  */
@@ -5,7 +14,7 @@
 namespace useless {
     
     /**
-     * Converts red, green, blue channels into a RGB color
+     * Sends some RGB values over websocket client
      * @param red value of the red channel between 0 and 255. eg: 255
      * @param green value of the green channel between 0 and 255. eg: 255
      * @param blue value of the blue channel between 0 and 255. eg: 255
@@ -15,7 +24,19 @@ namespace useless {
     //% advanced=true
     export function doBasicallyNothing(red: number, green: number, blue: number) {
         //return packRGB(red, green, blue);
-        console.log(`R:${red} G:${green} B:${blue}`)
+        //console.log(`R:${red} G:${green} B:${blue}`)
+
+        var wsconn = new WebSocket("wss://echo.websocket.org/");
+
+        wsconn.onopen = function(ev) {
+            WsOpen(ev);
+            console.log("Sending message to ws server");
+            wsconn.send(`R:${red} G:${green} B:${blue}`);
+        }
+
+        wsconn.onmessage = function (ev) {
+            WsOnMessage(ev);
+        }
     }
 
 }
