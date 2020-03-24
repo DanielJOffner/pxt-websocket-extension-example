8d58f8d436a173ca2b791a1d20f1d96a1f385530
unknown <daniel.offner@outlook.com>
2020-03-24 20:45:33 +1000

    doing nothing

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Functions to do basically nothing
+ */
+namespace useless {
+    
+    /**
+     * Converts red, green, blue channels into a RGB color
+     * @param red value of the red channel between 0 and 255. eg: 255
+     * @param green value of the green channel between 0 and 255. eg: 255
+     * @param blue value of the blue channel between 0 and 255. eg: 255
+     */
+    //% weight=1
+    //% blockId="doBasicallyNothing" block="red %red|green %green|blue %blue"
+    //% advanced=true
+    export function doBasicallyNothing(red: number, green: number, blue: number) {
+        //return packRGB(red, green, blue);
+        console.log(`R:${red} G:${green} B:${blue}`)
+    }
+
+}
